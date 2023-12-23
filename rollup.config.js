f0db1a1454d79f98c4bc9c7870e114e99be7fb63
Gaurav Kale <kalegd@dukes.jmu.edu>
2023-12-23 17:57:18 +0700

    Create initial project with building via rollup, linting with eslint, and setup as npm project

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import rootImport from 'rollup-plugin-root-import';
+import terser from '@rollup/plugin-terser';
+
+function header() {
+    return {
+        renderChunk(code) {
+            return `/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+${ code }`;
+        }
+    };
+}
+
+export default {
+    input: 'scripts/DigitalBacon-UI.js',
+    output: [{
+            file: 'build/DigitalBacon-UI.js',
+            format: 'es',
+        }, {
+            file: 'build/DigitalBacon-UI.min.js',
+            format: 'es',
+            name: 'version',
+            plugins: [
+                terser({mangle: { keep_classnames: true, keep_fnames: true }}),
+                header()
+            ],
+        },
+    ],
+    external: [
+        'three',
+    ],
+    plugins: [
+        rootImport({
+            // Will first look in `client/src/*` and then `common/src/*`.
+            root: `${__dirname}`,
+            useInput: 'prepend',
+
+            // If we don't find the file verbatim, try adding these extensions
+            extensions: '.js',
+        }),
+    ],
+};
---
+++ scripts/DigitalBacon-UI.js
@@ -0,0 +1,6 @@
+import * as TroikaThreeText from '/node_modules/troika-three-text/dist/troika-three-text.umd.js';
+
+const version = '0.0.1';
+
+export { TroikaThreeText };
+export { version };
