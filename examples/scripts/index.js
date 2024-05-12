93719dbca4b2345e71b9467151dd5d9cbbac5527
Gaurav Kale <kalegd@dukes.jmu.edu>
2024-05-12 08:39:02 +0530

    Add button to link to github code for examples

--- examples/scripts/index.js
+++ examples/scripts/index.js
@@ -1,10 +1,14 @@
 let selectedItem;
 let iframe = document.getElementById('viewer');
+let exampleCodeLink = document.getElementById('example-code-link');
 function chooseExample(li) {
     iframe.setAttribute('src', li.getAttribute('href'));
     if(selectedItem) selectedItem.classList.remove('selected');
     selectedItem = li;
     selectedItem.classList.add('selected');
+    exampleCodeLink.classList.remove('selected');
+    exampleCodeLink.href = 'https://github.com/kalegd/DigitalBacon-UI/blob/main'
+        + li.getAttribute('href') + '.html';
 }
 let items = document.querySelectorAll('.sidebar li');
 items.forEach((li) => li.addEventListener('click', () => chooseExample(li)));
