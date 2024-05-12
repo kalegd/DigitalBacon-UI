9c7983bfc8452560931c1e12c28ce84c9912daf6
Gaurav Kale <kalegd@dukes.jmu.edu>
2024-05-12 11:04:42 +0530

    Make sidebar responsive to mobile layout and add a sidebar slider button for mobile users

--- examples/scripts/index.js
+++ examples/scripts/index.js
@@ -1,6 +1,10 @@
 let selectedItem;
+let sidebarOpen = false;
 let iframe = document.getElementById('viewer');
+let iframeParent = document.getElementById('iframe-parent');
 let exampleCodeLink = document.getElementById('example-code-link');
+let sidebarSliderButton = document.getElementById('sidebar-slider-button');
+let sidebar = document.getElementById('sidebar');
 function chooseExample(li) {
     iframe.setAttribute('src', li.getAttribute('href'));
     if(selectedItem) selectedItem.classList.remove('selected');
@@ -12,6 +16,18 @@ function chooseExample(li) {
 }
 let items = document.querySelectorAll('.sidebar li');
 items.forEach((li) => li.addEventListener('click', () => chooseExample(li)));
+sidebarSliderButton.addEventListener('click', () => {
+    if(sidebarOpen) {
+        iframeParent.classList.remove('compress');
+        sidebar.classList.remove('open');
+        sidebarSliderButton.innerText = '>';
+    } else {
+        iframeParent.classList.add('compress');
+        sidebar.classList.add('open');
+        sidebarSliderButton.innerText = '<';
+    }
+    sidebarOpen = !sidebarOpen;
+});
 window.onmessage = function(e) {
     if(typeof e.data == 'string') {
         window.location.href = e.data;
